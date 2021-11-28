3bf86d395c4bb9f42a927ce546bb70b4fbdbcc0d
Yuxin Liu <liu.yuxin1@northeastern.edu>
2021-11-27 23:05:50 -0500

    unicode to pinyin

--- src/main/java/edu/neu/coe/info6205/sort/counting/PinyinConverter.java
+++ src/main/java/edu/neu/coe/info6205/sort/counting/PinyinConverter.java
@@ -20,17 +20,54 @@ import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombi
  */
 public class PinyinConverter {
     
+    //unicode to pinyin without tone eg: \u548c to he
+    public static String unicodeToPinyinNOTone(String str) throws BadHanyuPinyinOutputFormatCombination {
+        char ch = unicodeToChar(str);
+        return charToPinyinNOTone(ch);
+    }
     
+    //unicode to pinyin with tone as a number eg: \u548c to he2
+    public static String unicodeToPinyinWITone(String str) throws BadHanyuPinyinOutputFormatCombination {
+        char ch = unicodeToChar(str);
+        return charToPinyinWITone(ch);
+    }
+    
+    //unicode to char
     public static char unicodeToChar(String str) {
         Integer code = Integer.parseInt(str.substring(2), 16);
         char ch = Character.toChars(code)[0];
         return ch;
     }
     
-    public static String charToPinyin(char ch) throws BadHanyuPinyinOutputFormatCombination {
+    //char to pinyin without tone
+    public static String charToPinyinNOTone(char ch) throws BadHanyuPinyinOutputFormatCombination {
         HanyuPinyinOutputFormat outputFormat = new HanyuPinyinOutputFormat();
         outputFormat.setVCharType(HanyuPinyinVCharType.WITH_V);
         outputFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
         return PinyinHelper.toHanyuPinyinStringArray(ch, outputFormat)[0];
+    }
+    
+    //char to pinyin with tone
+    public static String charToPinyinWITone(char ch) throws BadHanyuPinyinOutputFormatCombination {
+        HanyuPinyinOutputFormat outputFormat = new HanyuPinyinOutputFormat();
+        outputFormat.setVCharType(HanyuPinyinVCharType.WITH_V);
+        outputFormat.setToneType(HanyuPinyinToneType.WITH_TONE_NUMBER);
+        return PinyinHelper.toHanyuPinyinStringArray(ch, outputFormat)[0];
+    }
+    
+    //sample test \u548c to 和 to he/he2
+    public static void main(String[] args) throws BadHanyuPinyinOutputFormatCombination {
+        String test = "\\u548c";
+        System.out.println(test);
+        char ch = unicodeToChar(test);
+        System.out.println(ch);
+        String py1 = charToPinyinNOTone(ch);
+        System.out.println(py1);
+        String py2 = charToPinyinWITone(ch);
+        System.out.println(py2);
+        String py3 = unicodeToPinyinNOTone(test);
+        System.out.println(py3);
+        String py4 = unicodeToPinyinWITone(test);
+        System.out.println(py4);
     }
 }
